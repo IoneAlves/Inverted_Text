60f7464612cd12ae6b26f3f520b716fff785cca2
Ione Alves <ione.alves@ymail.com>
2022-11-18 22:57:21 -0300

    Finished function to invert the text and started to do the css styles

--- index.js
+++ index.js
@@ -4,27 +4,10 @@ console.log(invertedText.value)
 
 let newWord = [];
 let letters = '';
-let letter = ''
+let letter = '';
 let word = '';
 
 function keyPress() {
     let text = document.querySelector('#text').value;
-    if(text != "") {
-        newWord.push(text);
-        if(newWord != ''){
-            letter = newWord[newWord.length - 1]
-            word.concat(letter,letters)
-            invertedText.value = word.value;
-        }
-    }   
+    invertedText.textContent = text.split('').reverse().join('');
 }
-
-// let letters = text.value.split('')
-// let newWord = '';
-
-// for (let index = 1; index <= letters.length; index++) {
-//     let element = letters[letters.length - index];
-//     newWord = newWord + element;
-// }
-
-// console.log(newWord);    
