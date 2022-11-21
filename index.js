57f47399f6fcff2259106c8690081f4d35f76dd3
Ione Alves <ione.alves@ymail.com>
2022-11-20 23:48:44 -0300

    Finished styles and project

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 const invertedText = document.querySelector('#invertedText');
 
-console.log(invertedText.value)
-
-let newWord = [];
-let letters = '';
-let letter = '';
-let word = '';
-
 function keyPress() {
     let text = document.querySelector('#text').value;
     invertedText.textContent = text.split('').reverse().join('');
